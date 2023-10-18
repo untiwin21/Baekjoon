a96a9e3de8b84f1fa4c4e12891c8551ddeeb19d2
공학계열/최태웅 <taewoong1377@g.skku.edu>
2023-10-18 07:42:09 +0000

    1000.cpp

---
+++ 백준/Bronze/1000.A+B/1000.cpp
@@ -0,0 +1,9 @@
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+#include <iostream>	//헤더파일 포함할 때 확장자뺀다
+using namespace std;
+int main(){
+	int a, b;	//변수 2개 만들기
+	cin>>a>>b;	//변수 2개 한 번에 입력받기
+	cin<<a+b;	//변수 2개 합 출력하기
+	return 0;
+}
